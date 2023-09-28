b0f383ce9b5bb51c4830b430fe030d21c5dccd46
qhtkdrma <kimwonkyu15@gmail.com>
2023-09-28 11:18:38 +0900

    create BoardService

---
+++ spirngCRUD/src/main/java/com/learn/service/BoardServiceImpl.java
@@ -0,0 +1,40 @@
+package com.learn.service;
+
+import java.util.List;
+import javax.inject.Inject;
+import org.springframework.stereotype.Service;
+
+import com.learn.domain.BoardVO;
+import com.learn.persistence.BoardDAO;
+
+public class BoardServiceImpl implements BoardService {
+	@Inject
+	private BoardDAO dao;
+	
+	
+	@Override
+	public void regist(BoardVO board) throws Exception {
+		dao.create(board);
+	}
+
+	@Override
+	public BoardVO read(Integer bno) throws Exception {
+		return dao.read(bno);
+	}
+
+	@Override
+	public void modify(BoardVO board) throws Exception {
+		dao.update(board);
+	}
+
+	@Override
+	public void remove(Integer bno) throws Exception {
+		dao.delete(bno);
+	}
+
+	@Override
+	public List<BoardVO> listAll() throws Exception {
+		return dao.listAll();
+	}
+
+}
